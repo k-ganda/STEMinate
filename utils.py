39efa3f5f011c88b66264391089477a958c94e35
m-mwangi <127857750+m-mwangi@users.noreply.github.com>
2024-07-30 22:52:27 +0000

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-try:
-    db.create_all()
-except Exception as e:
-    print(f"Error creating database: {e}")
-    # Consider logging the error or taking other actions
