dfc0471caa326eb7e1f6c2710ba0e5469510afdd
k-ganda <116561806+k-ganda@users.noreply.github.com>
2024-07-31 06:30:35 -0700

    Update utils.py

--- back_end/utils.py
+++ back_end/utils.py
@@ -1,9 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
-
-try:
-    db.create_all()
-except Exception as e:
-    print(f"Error creating database: {e}")
-    # Consider logging the error or taking other actions
