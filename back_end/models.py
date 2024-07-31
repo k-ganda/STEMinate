e3984b6d66428f9afd3bfd0b0276cab8e3a6f925
k-ganda <116561806+k-ganda@users.noreply.github.com>
2024-07-31 05:11:04 -0700

    Update models.py

--- back_end/models.py
+++ back_end/models.py
@@ -6,7 +6,7 @@ from flask_login import UserMixin
 # ... other imports ... (if any)
 
 
-class User(UserMixin):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
