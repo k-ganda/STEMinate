632939cdde88e402d3294d18b1d3117f6750e946
k-ganda <116561806+k-ganda@users.noreply.github.com>
2024-07-31 04:11:45 -0700

    Update main.py

--- back_end/main.py
+++ back_end/main.py
@@ -1,9 +1,8 @@
-from app import create_app
+from backend import create_app
 from flask_socketio import SocketIO
 
+app = create_app()
+socketio = SocketIO(app)  # Access SocketIO instance from create_app
 
 if __name__ == '__main__':
-    app = create_app()
-    socketio = SocketIO(app)  # Access SocketIO instance from create_app
     socketio.run(app, debug=True)
-    
